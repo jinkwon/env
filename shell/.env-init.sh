ef0376738d60d2b5527a394c620724984a4df5c8
JK <uzmystic@gmail.com>
2021-08-14 11:55:12 +0900

    Update .env-init.sh

--- shell/.env-init.sh
+++ shell/.env-init.sh
@@ -1,7 +1,12 @@
-brew install zsh-syntax-highlighting
+rm -rf ~/.oh-my-zsh/themes/powerlevel10k
+rm -rf ~/._env/zsh-syntax-highlighting
 git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/._env/zsh-syntax-highlighting
 
+# set custom zsh config
 LINE='[[ ! -f ~/._env/custom-config.zsh ]] || source ~/._env/custom-config.zsh'
-FILE='~/.zshrc'
+FILE='./.zshrc'
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 
+# replace theme
+sed -i '' 's/ZSH_THEME=.*$/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
