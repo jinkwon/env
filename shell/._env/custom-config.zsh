12b3a908228977fd198b42b8d7cced6d90056f2b
JK <uzmystic@gmail.com>
2021-08-14 12:13:36 +0900

    Update custom-config.zsh

--- shell/._env/custom-config.zsh
+++ shell/._env/custom-config.zsh
@@ -14,3 +14,7 @@ kn(){
 }
 
 source ~/._env/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
