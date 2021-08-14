4f8bf824912d33dfaea2976fd14ad8e5f48e4aa6
jinkwon.lee <uzmystic@gmail.com>
2021-08-14 16:02:53 +0900

    update config

--- shell/.custom-config.zsh
+++ shell/.custom-config.zsh
@@ -4,22 +4,29 @@ export NVM_DIR="$HOME/.nvm"
 source ~/._env/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source ~/._env/zsh-autosuggestions/zsh-autosuggestions.zsh
 
+# yarn
 alias ycu="yarn-check -u"
 alias yi="yarn install"
 alias ys="yarn run start"
 alias yd="yarn run dev"
 alias yb="yarn run build"
 
+# git
 alias ggcc="git branch --merged | grep -v \* | xargs git branch -D "
 alias gld="git pull origin dev && ggcc"
 alias gp="git push origin $1"
 alias gpf="git push origin $1 --force"
 alias gcam="git commit -am $1"
+
+# ssh
 alias ssh-ad="ssh ubuntu@unicorn.alpha.db"
 alias ssh-as="ssh ubuntu@unicorn.alpha.server"
-alias kp="lsof -t -i tcp:11002 | xargs kill -9"
-alias myhelp="ccat ~/.custom-config.zsh"
 
+# kill
+alias kp="lsof -t -i tcp:$1 | xargs kill -9"
 kn(){
   kill -9 `ps -ef | grep $1 | awk "{print $2}"`
 }
+
+# etc
+alias myhelp="ccat ~/.custom-config.zsh"
