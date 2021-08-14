8e193be968014ceab912bf58c983fd31be08a313
jinkwon.lee <uzmystic@gmail.com>
2021-08-14 13:11:26 +0900

    update settings

--- shell/.custom-config.zsh
+++ shell/.custom-config.zsh
@@ -8,7 +8,7 @@ alias ggcc="git branch --merged | grep -v \* | xargs git branch -D "
 alias ssh-ad="ssh ubuntu@unicorn.alpha.db"
 alias ssh-as="ssh ubuntu@unicorn.alpha.server"
 alias kp="lsof -t -i tcp:11002 | xargs kill -9"
-alias myhelp="cat ~/._env/custom-alias.zsh"
+alias myhelp="cat ~/.custom-config.zsh"
 kn(){
   kill -9 `ps -ef | grep $1 | awk '{print $2}'`
 }
