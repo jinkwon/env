580aa7e1b3a51a53d0d37ff9e45c6251f0e9179f
JK <uzmystic@gmail.com>
2021-08-14 10:37:44 +0900

    Update and rename shell/.env-init.sh to shell/._env/env-init.sh

---
+++ shell/._env/env-init.sh
@@ -0,0 +1,3 @@
+LINE='[[ ! -f ~/._env/custom-alias.zsh ]] || source ~/._env/custom-alias.zsh"'
+FILE='../.zshrc'
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
--- shell/.env-init.sh
+++
@@ -1,1 +0,0 @@
-echo "[[ ! -f ~/._env/custom-alias.zsh ]] || source ~/._env/custom-alias.zsh" >> .zshrc
