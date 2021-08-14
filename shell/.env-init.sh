722d5f1749d459bba68171353a1fc687df662737
JK <uzmystic@gmail.com>
2021-08-14 12:16:23 +0900

    Update .env-init.sh

--- shell/.env-init.sh
+++ shell/.env-init.sh
@@ -2,6 +2,7 @@ rm -rf ~/.oh-my-zsh/themes/powerlevel10k
 rm -rf ~/._env/zsh-syntax-highlighting
 git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/._env/zsh-syntax-highlighting
+mkdir ~/.nvm
 
 # set custom zsh config
 LINE='[[ ! -f ~/._env/custom-config.zsh ]] || source ~/._env/custom-config.zsh'
