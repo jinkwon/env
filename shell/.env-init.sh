b41c6986eb486697e96769788c9006ce45fad9af
JK <uzmystic@gmail.com>
2021-08-14 10:56:01 +0900

    Update .env-init.sh

--- shell/.env-init.sh
+++ shell/.env-init.sh
@@ -1,5 +1,7 @@
+brew install zsh-syntax-highlighting
+git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
+
 LINE='[[ ! -f ~/._env/custom-config.zsh ]] || source ~/._env/custom-config.zsh'
 FILE='~/.zshrc'
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 
-brew install zsh-syntax-highlighting
