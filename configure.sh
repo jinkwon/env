3941a5180eafecbd86aa86f0b8d24b89bac72984
jinkwon.lee <uzmystic@gmail.com>
2021-08-17 22:57:24 +0900

    feat: update env

--- configure.sh
+++ configure.sh
@@ -6,6 +6,7 @@ initConfig() {
   LINE='[[ ! -f ~/.custom-config.zsh ]] || source ~/.custom-config.zsh'
   grep -qF -- "$LINE" ~/.zshrc || echo "$LINE" >> ~/.zshrc
   sed -i '' 's/ZSH_THEME=.*$/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
+
   echo "==setting completed ==="
 }
 
--- shell/.custom-base.zsh
+++ shell/.custom-base.zsh
@@ -2,5 +2,6 @@ export NVM_DIR="$HOME/.nvm"
 export NVM_DIR="$HOME/.nvm"
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-source ~/._env/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source ~/._env/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /opt/homebrew/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /opt/homebrew//zsh-autosuggestions/zsh-autosuggestions.zsh
+source /opt/homebrew/share/autojump/autojump.fish
