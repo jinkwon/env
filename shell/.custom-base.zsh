9570c8cfa415191bd7c650ec8bae273588a58661
jinkwon <uzmystic@gmail.com>
2021-08-18 03:08:28 +0900

    task: added homebrew prefix

--- shell/.custom-base.zsh
+++ shell/.custom-base.zsh
@@ -1,7 +1,10 @@
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+brewPrefix=$(brew --prefix)
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "${brewPrefix}/opt/nvm/nvm.sh" ] && . "${brewPrefix}/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "${brewPrefix}/opt/nvm/etc/bash_completion.d/nvm" ] && . "${brewPrefix}/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
 source ~/._env/alias-tips/alias-tips.plugin.zsh
-[ -f /opt/homebrew/etc/profile.d/autojump.sh ] && . /opt/homebrew/etc/profile.d/autojump.sh
+source "${brewPrefix}"/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source "${brewPrefix}"/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+[ -f "${brewPrefix}"/etc/profile.d/autojump.sh ] && . "${brewPrefix}"/etc/profile.d/autojump.sh
