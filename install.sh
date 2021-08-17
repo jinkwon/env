0dd228f5247d8a18dfa77b734265027c22532c4f
jinkwon <uzmystic@gmail.com>
2021-08-18 03:11:45 +0900

    feat: added shell script

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ mkdir -p ~/.nvm
 rm -rf ~/._env ~/.oh-my-zsh/themes/powerlevel10k
 git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
 git clone https://github.com/djui/alias-tips.git ~/._env/alias-tips
-/opt/homebrew/opt/fzf/install
+brewPrefix=$(brew --prefix)
+"${brewPrefix}"/opt/fzf/install
