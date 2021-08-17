b04bb05dac7088e6540c8a1c303c641d014eb883
jinkwon.lee <uzmystic@gmail.com>
2021-08-18 00:10:10 +0900

    feat: update env

--- configure.sh
+++ configure.sh
@@ -44,5 +44,3 @@ initGit() {
 initConfig
 readFile
 askCommand
-cd ~
-source ~/.zshrc
--- shell/.custom-base.zsh
+++ shell/.custom-base.zsh
@@ -1,8 +1,6 @@
-export NVM_DIR="$HOME/.nvm"
-export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/usr/local/opt/
-nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source ~/._env/alias-tips/alias-tips.plugin.zsh
