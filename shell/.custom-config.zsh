e13646879fc0d209488720d1702e9feb035e6a21
jinkwon.lee <uzmystic@gmail.com>
2021-08-17 10:49:46 +0900

    feat: clean up codes

--- shell/.custom-config.zsh
+++ shell/.custom-config.zsh
@@ -1,16 +1,10 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-source ~/._env/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source ~/._env/zsh-autosuggestions/zsh-autosuggestions.zsh
-
+source ~/.custom-base.zsh
 # yarn
 alias ycu="yarn-check -u"
 alias yi="yarn install"
 alias ys="yarn run start"
 alias yd="yarn run dev"
 alias yb="yarn run build"
-
 # git
 alias gclr="git branch --merged | grep -v \* | xargs git branch -D "
 alias gld="git pull origin dev && gclr && git fetch -p"
@@ -19,11 +13,9 @@ alias gl="git pull origin $1"
 alias gp="git push origin $1"
 alias gpf="git push origin $1 --force"
 alias gcam="git commit -am $1"
-
 # ssh
 alias ssh-ad="ssh ubuntu@unicorn.alpha.db"
 alias ssh-as="ssh ubuntu@unicorn.alpha.server"
-
 # kill
 kp(){
   lsof -t -i tcp:$1 | xargs kill -9
@@ -31,6 +23,5 @@ kp(){
 kn(){
   kill -9 `ps -ef | grep $1 | awk '{print $2}'`
 }
-
 # etc
 alias menu="ccat ~/.custom-config.zsh"
