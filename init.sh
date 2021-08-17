5e3a262b50a50068ba5e65715773180dc7c36ce6
jinkwon.lee <uzmystic@gmail.com>
2021-08-17 22:46:51 +0900

    feat: update env

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
-brew install ccat fzf wget nvm jq
+./install.sh
 ./configure.sh
 mkdir -p ~/.nvm
 rm -rf ~/._env ~/.oh-my-zsh/themes/powerlevel10k
 git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/._env/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/._env/zsh-autosuggestions
