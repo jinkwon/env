8c258fc02ecfcef4f853dc13406daf2a261daa84
jinkwon <uzmystic@gmail.com>
2021-08-20 09:19:32 +0900

    feat: clean up config file

--- shell/.custom-config.zsh
+++ shell/.custom-config.zsh
@@ -7,6 +7,7 @@ alias yd="yarn run dev"
 alias yb="yarn run build"
 alias ya="yarn add $1 $2"
 # git
+alias grrc="git rm -r --cached ."
 alias gclr="git branch --merged | grep -v \* | xargs git branch -D "
 alias gd="git pull origin dev && gclr && git fetch -p"
 alias gs="git status"
@@ -24,4 +25,4 @@ _kn(){ kill -9 `ps -ef | grep $1 | awk '{print $2}'`; }
 # etc
 alias _menu="ccat ~/.custom-config.zsh"
 alias _port="lsof -PiTCP -sTCP:LISTEN"
-_dockerrmf() { docker stop $1;docker rm $1; }
+_dockerrmf(){ docker stop $1;docker rm $1; }
